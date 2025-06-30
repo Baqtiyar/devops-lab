c04da56678a05d7c3a2a495b70b01539926c499e
Baqtiyar <Baqtiyarkhan01@gmail.com>
2025-06-30 04:51:11 +0000

    added newfiles and renamed a few

---
+++ Scripting/Shell-Scripting/backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+<<Disclaimer
+This script will take backups from source to target
+In future we will do cronjob as well
+Disclaimer
+
+
+src_dir=/home/ec2-user/californiagit4/Scripting/Shell-Scripting
+tgt_dir=/home/ec2-user/californiagit4/Scripting/Shell-Scripting/backups
+
+backup_filename= "backup_$(date).tar.gz" 
+
+echo "Backup Started"
+
+echo "Backing up to $backup_filename..."
+
+tar -czvf "${tgt_dir}/${backup_filename}" "$sr_dir"
+
+echo "Backup complete"
